a771271dcdf748e05e3235156cb1f0ee00e51cea
faustin lies <faustinlokuta@gmail.com>
2022-09-06 02:34:51 +0200

    établissement de la connexion avec le serveur de la base des données

--- user.php
+++ user.php
@@ -1,3 +1,51 @@
+<?php
+// la variable qui va contenir la connexion à la base des données
+$conn=mysqli_connect("localhost","root","","kenerozia");
+/*if ($conn) {
+    echo '<p class="text-danger shadow text-center">connection reussie</p>';
+}*/
+    if (isset($_POST['send'])){
+        $nom= $_POST["nom"];
+        $prenom= $_POST["prenom"];
+        $sex=$_POST["sexe"];
+        $email= $_POST["email"];
+        $phone= $_POST["phone"];
+        $adress= $_POST["adress"];
+        $mdp= $_POST["mdp"];
+        $file_name=$_FILES['photo']['name'];
+        $file_tmp=$_FILES['photo']['tmp_name'];
+        $file_size=$_FILES['photo']['size'];
+        $file_error=$_FILES['photo']['error'];
+        $file_type=$_FILES['photo']['type'];
+	  
+	  if(is_uploaded_file($file_tmp)){
+			if($file_size < 2097152){
+				if($file_type= 'image/jpg'){
+					if(move_uploaded_file($file_tmp, "uploads/$file_name")){
+						$output= '<p class="alert alert-success">file uploaded successefully</p>';
+					}else{
+						$output= '<p class="alert alert-success">file uploaded failed</p>';
+					}
+					}else{
+						$output= '<p class="alert alert-success">file not image</p>';
+					}
+					}else{
+						$output= '<p class="alert alert-success">2M only</p>';
+					}
+					}else{
+						$output= '<p class="alert alert-success">Please select a file</p>';
+					}
+                 
+            $query="INSERT INTO utilisateur(id,nom,prenom,sexe,email,phone,adresse,photo,mdp) 
+            VALUES(NULL,'$nom','$prenom','$sex','$email','$phone','$adress','$mdp','$file_name')";
+
+            $send= mysqli_query($conn,$query);
+            //cette fonction prend deux paramettres() et cette fonction permet d'envoyé les informations qui viennnent de la db
+    }
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,19 +56,19 @@
     <link rel="stylesheet" href="vendor/fontawesome-free/all.css">
     <title>Kenerozia</title>
 </head>
-<body>
+<body class="bg-dark">
 <div class="row">
 <div class="offset-lg-2 col-lg-10 offset-lg-2 m-5">
 <form action="" method="Post" enctype="multipart/form-data">
-        <h1 class="text-warning"> formulaire d'inscription de l'admit</h1>
+        <h1 class="text-warning"> formulaire d'inscription de l'utilisateur</h1>
         <div class="col-lg-6">
         <small class="text-primary"> entrez votre nom</small>
         <input type="text" name=nom id="nom" class = form-control>
         <small class="text-primary"> entrez votre prenom</small>
         <input type="text" name="prenom" id="prenom" class = form-control>
         <small class="text-primary"> entrez votre sexe</small>
-        <Select name="sexe" id="sexe">
-                <option value=" "> </option>
+        <Select name="sexe" id="sexe" class = form-control>
+                <option value=" ">  </option>
                 <option value="M">Masculin</option>
             	<option value="F">Feminin</option>
         </select>
@@ -35,7 +83,7 @@
         <input type="file" name="photo" id="photo" class = form-control>
         <small class="text-primary"> entrez votre mot de pass</small>
         <input type="password" name="mdp" id="mdp" class = form-control>
-        <input type="submit" value="send" name="send" class="btn-info m-2">
+        <input type="submit" value="send" name="send" class="btn btn-primary m-2">
         </div>
 </form>
 
